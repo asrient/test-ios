4ff33a9d50ea2405e0bfb7a0c89a9301c7a728b6
Asrient <asen182000@hotmail.com>
2020-06-17 19:18:42 +0530

    test lists

--- Clock/ContentView.swift
+++ Clock/ContentView.swift
@@ -9,41 +9,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showAlert: Bool = false
+    
     var body: some View {
-            VStack() {
-                Text("This is your iPhone")
-                    .font(.body)
-                    .fontWeight(.regular)
-                    .foregroundColor(Color.gray)
-                    .padding(.bottom, 50)
-                    Button(action: {
-                        self.showAlert=true
-                    }) {
-                        Text("Cute")
-                            .font(.headline)
-                        .fontWeight(.regular)
-                            .foregroundColor(Color.orange)
-                    }
-                
-            }
-            .sheet(isPresented: $showAlert) {
-                VStack() {
-                Text("Cuteness")
-                .font(.title)
-                    .padding(.bottom,30)
-                  Button(action: {
-                 self.showAlert=false
-                  }) {
-                   Text("Close")
-                       .font(.headline)
-                       .fontWeight(.regular)
-                      .foregroundColor(Color.red)
-                    }
-                    
-                }
-                }
+       ListView()
     }
+}
+
+struct Pokemon: Identifiable {
+  let id: Int
+  let name: String
+  let type: String
+  let color: Color
+}
+
+struct ListView : View {
+    @State private var showAlert: Bool = false
+    @State var pokemonList = [
+    Pokemon(id: 0, name: "Charmander", type: "Fire", color: .red),
+    Pokemon(id: 1, name: "Squirtle", type: "Water", color: .blue),
+    Pokemon(id: 2, name: "Bulbasaur", type: "Grass", color: .green),
+    Pokemon(id: 3, name: "Pikachu", type: "Electric", color: .yellow),
+  ]
+  var body: some View {
+    NavigationView {
+      List(pokemonList) { pokemon in
+        HStack {
+          Text(pokemon.name)
+            Spacer()
+          Text(pokemon.type).foregroundColor(pokemon.color)
+        }
+      }
+      .navigationBarTitle(Text("Pokemon"))
+      .navigationBarItems(
+        trailing: HStack{
+            Button(action: {
+                    self.showAlert=true
+                })
+            {
+              Text("Cute")
+                .font(.headline)
+             .fontWeight(.regular)
+                .foregroundColor(Color.orange)
+                .padding(.trailing)
+                }
+            Button(action: addPokemon, label: { Text("Add")
+                .foregroundColor(Color.orange)
+            })
+        }
+      )
+    }
+    .sheet(isPresented: $showAlert) {
+        VStack() {
+        Text("Cuteness")
+        .font(.title)
+            .padding(.bottom,30)
+          Button(action: {
+         self.showAlert=false
+          }) {
+           Text("Close")
+               .font(.headline)
+               .fontWeight(.regular)
+              .foregroundColor(Color.red)
+            }
+            
+        }
+        }  }
+  func addPokemon() {
+    if let randomPokemon = pokemonList.randomElement() {
+      pokemonList.append(randomPokemon)
+    }
+  }
 }
 
 struct preview: PreviewProvider {
